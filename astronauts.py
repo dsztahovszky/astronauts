b24481cdfceae160f1739c7fde34008ea8948115
dsztahovszky <dsztahovszky@gmail.com>
2023-03-05 14:03:34 +0100

    last commit

--- astronauts.py
+++ astronauts.py
@@ -1,17 +1,29 @@
 import csv
 
 
-def main():
+def get_datas():
     with open('astronauts.csv', newline='') as astr_file:
         datas_csvreader = csv.reader(astr_file)
-        datas = [data for data in datas_csvreader]
+        return [data for data in datas_csvreader]
+
+
+def get_birth_months():
+    datas = get_datas()
     birthdate_index = datas[0].index('Birth Date')
-    birthdate_months = []
+    birth_months = []
     for astronaut in datas[1:]:
-        birthdate_months.append(int(astronaut[birthdate_index].split('/')[0]))
+        birth_months.append(int(astronaut[birthdate_index].split('/')[0]))
+    return birth_months
+
+
+def make_months_list(birth_months):
     months = []
     for index in range(1, 13):
-        months.append(birthdate_months.count(index))
+        months.append(birth_months.count(index))
+    return months
+
+
+def make_most_monts_datas(months):
     all_months = months.copy()
     most_months = []
     most_months.insert(0, [months.index(max(months)), round((max(months) / sum(all_months)) * 100, 1)])
@@ -31,24 +43,45 @@ def main():
             most_months.insert(2, [months.index(max(months)) + 1, round((max(months) / sum(all_months)) * 100, 1)])
     else:
         most_months.insert(2, [months.index(max(months)), round((max(months) / sum(all_months)) * 100, 1)])
-    months_words = {0: 'Január',
-                    1: 'Február',
-                    2: 'Március',
-                    3: 'Április',
-                    4: 'Május',
-                    5: 'Június',
-                    6: 'Július',
-                    7: 'Augusztus',
-                    8: 'Szeptember',
-                    9: 'Október',
-                    10: 'November',
-                    11: 'December'}
+    return most_months
+
+
+def make_months_words():
+    return {0: 'Január',
+            1: 'Február',
+            2: 'Március',
+            3: 'Április',
+            4: 'Május',
+            5: 'Június',
+            6: 'Július',
+            7: 'Augusztus',
+            8: 'Szeptember',
+            9: 'Október',
+            10: 'November',
+            11: 'December'}
+
+
+def pair_months_words(most_months):
+    months_words = make_months_words()
     for month in most_months:
         month[0] = months_words[month[0]]
-    print('A három leggyakoribb születési hónap a NASA űrhajósai körében százalékos arányokkal kiegészítve:',)
-    print(most_months[0][0], '; ', most_months[0][1], '%')
-    print(most_months[1][0], '; ', most_months[1][1], '%')
-    print(most_months[2][0], '; ', most_months[2][1], '%')
+    return most_months
+
+
+def print_datas(datas):
+    print('A három leggyakoribb születési hónap a NASA űrhajósai körében százalékos arányokkal kiegészítve:',
+          end='\n\n')
+    print(datas[0][0], '; ', datas[0][1], '%')
+    print(datas[1][0], '; ', datas[1][1], '%')
+    print(datas[2][0], '; ', datas[2][1], '%')
+
+
+def main():
+    birth_months = get_birth_months()
+    months = make_months_list(birth_months)
+    most_months = make_most_monts_datas(months)
+    pair_months_words(most_months)
+    print_datas(most_months)
 
 
 main()
