aa4724b491a4496b5455f4f914dfbe82a7ae0897
dsztahovszky <dsztahovszky@gmail.com>
2023-03-05 11:06:36 +0100

    print birth months 2

--- astronauts.py
+++ astronauts.py
@@ -8,8 +8,11 @@ def main():
     birthdate_index = datas[0].index('Birth Date')
     birthdate_months = []
     for astronaut in datas[1:]:
-        birthdate_months.append(astronaut[birthdate_index].split('/')[0])
-    print(birthdate_months)
+        birthdate_months.append(int(astronaut[birthdate_index].split('/')[0]))
+    months = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0, 11: 0, 12: 0}
+    for index in range(1, 13):
+        months[index] = birthdate_months.count(index)
+    print(months)
 
 
 main()
